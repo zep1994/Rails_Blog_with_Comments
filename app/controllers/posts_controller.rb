aba7b10614423d59ab5cb04ba33eb656d49b40ea
zep1994 <snaxbags@protonmail.com>
2019-01-31 23:34:09 -0600

    Implement CRUD

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,30 @@
 class PostsController < ApplicationController
+
     def index 
+        @posts = Post.all.order("created_at DESC")
+    end
+
+    def new 
+        @post = Post.new
+    end
+
+    def create
+        @post = Post.new(posts_params)
+        if @post.save 
+            redirect_to @post
+        else 
+            render 'new'
+        end
+    end
+
+    def show
+        @post = Post.find(params[:id])
+    end
+
+
+    private
+
+    def posts_params
+        params.require(:post).permit(:title, :content)
     end
 end
